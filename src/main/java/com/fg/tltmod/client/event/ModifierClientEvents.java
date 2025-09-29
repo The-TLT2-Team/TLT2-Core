361549985ea3a66ba80836fb95d3197fabefc334
ssakura49 <ssakura49@users.noreply.github.com>
2025-09-29 20:20:24 +0800

    修补

--- src/main/java/com/fg/tltmod/client/event/ModifierClientEvents.java
+++ src/main/java/com/fg/tltmod/client/event/ModifierClientEvents.java
@@ -9,6 +9,7 @@ import com.ssakura49.sakuratinker.api.entity.render.RenderEffects;
 import com.ssakura49.sakuratinker.utils.tinker.ToolUtil;
 import net.minecraft.client.Camera;
 import net.minecraft.client.Minecraft;
+import net.minecraft.util.Mth;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.api.distmarker.Dist;
@@ -28,31 +29,38 @@ public class ModifierClientEvents {
         Minecraft mc = Minecraft.getInstance();
         Player player = mc.player;
         if (player == null) return;
-
+        float partialTick = event.getPartialTick();
         player.getCapability(TinkerDataCapability.CAPABILITY).ifPresent(cap -> {
             int lvl = cap.get(BlessingFromFoxGod.KEY_BLESS, 0);
             if (lvl > 0) {
                 PoseStack poseStack = event.getPoseStack();
                 poseStack.pushPose();
 
+                double px = Mth.lerp(partialTick, player.xo, player.getX());
+                double py = Mth.lerp(partialTick, player.yo, player.getY());
+                double pz = Mth.lerp(partialTick, player.zo, player.getZ());
+
                 Camera camera = mc.gameRenderer.getMainCamera();
                 double camX = camera.getPosition().x;
                 double camY = camera.getPosition().y;
                 double camZ = camera.getPosition().z;
 
-                float yaw = player.getYRot();
+                float yaw = Mth.lerp(partialTick, player.yRotO, player.getYRot());
 
                 double distance = 0.5;
-                double offsetX = Math.sin(Math.toRadians(yaw)) * distance;
-                double offsetZ = -Math.cos(Math.toRadians(yaw)) * distance;
+                double rad = Math.toRadians(yaw + 180);
+                double offsetX = Math.sin(rad) * distance;
+                double offsetZ = -Math.cos(rad) * distance;
 
                 poseStack.translate(
-                        player.getX() - camX + offsetX,
-                        player.getY() - camY + player.getBbHeight() * 0.5, // 背后高度中间
-                        player.getZ() - camZ + offsetZ
+                        px - camX + offsetX,
+                        py - camY + player.getBbHeight() * 0.5,
+                        pz - camZ + offsetZ
                 );
 
                 poseStack.mulPose(Axis.YP.rotationDegrees(-yaw));
+                poseStack.mulPose(Axis.YP.rotationDegrees(180));
+                poseStack.translate(0.2,0,0.2);
 
                 RenderEffects.RegularPolygon.render(
                         poseStack,
