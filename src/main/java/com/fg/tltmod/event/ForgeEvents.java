93c9602d0dbe21b409ed8c6066e471426e957c37
ssakura49 <ssakura49@users.noreply.github.com>
2025-10-21 17:49:29 +0800

    咕

--- src/main/java/com/fg/tltmod/event/ForgeEvents.java
+++ src/main/java/com/fg/tltmod/event/ForgeEvents.java
@@ -1,29 +1,15 @@
 package com.fg.tltmod.event;
 
-import com.aizistral.enigmaticlegacy.api.capabilities.EnigmaticCapabilities;
 import com.aizistral.enigmaticlegacy.api.capabilities.IPlaytimeCounter;
-import com.aizistral.enigmaticlegacy.api.capabilities.PlayerPlaytimeCounter;
-import com.aizistral.enigmaticlegacy.handlers.SuperpositionHandler;
 import com.fg.tltmod.TltCore;
-import dev.xkmc.l2complements.init.registrate.LCEffects;
-import dev.xkmc.l2hostility.init.registrate.LHEffects;
-import net.minecraft.nbt.CompoundTag;
 import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.world.entity.Entity;
-import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
-import net.minecraftforge.event.AttachCapabilitiesEvent;
 import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.UUID;
-
 @Mod.EventBusSubscriber(modid = TltCore.MODID, bus = Mod.EventBusSubscriber.Bus.FORGE)
 public class ForgeEvents {
-    private static final Map<UUID, CompoundTag> CLONE_CACHE = new HashMap<>();
     @SubscribeEvent
     public static void onClone(PlayerEvent.Clone event) {
         Player newPlayer = event.getEntity();
@@ -36,3 +22,4 @@ public class ForgeEvents {
         }
 
     }
+}
